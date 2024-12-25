d6f6c4b6ae85ad545d9295adfe76f3bcd18263bd
esmaeili-m <mahdiesmaeili1011@gmail.com>
2024-12-26 00:45:54 +0330

    update

--- app/Livewire/Home/Auth/Login.php
+++ app/Livewire/Home/Auth/Login.php
@@ -48,7 +48,7 @@ class Login extends Component
         $response = json_decode($res->getBody()->getContents(), true);
         $token = $response['token'];
         $getMethod = '';
-        return view('payment.redirect', compact('token', 'getMethod'));
+        return view('livewire.home.redirect', compact('token', 'getMethod'));
     }
     public function generate_tr_code()
     {
