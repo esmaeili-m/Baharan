ea813e90f68853a114c44735c8415d266725cea6
esmaeili-m <mahdiesmaeili1011@gmail.com>
2024-12-26 01:10:23 +0330

    update

--- resources/views/livewire/home/redirect.blade.php
+++ resources/views/livewire/home/redirect.blade.php
@@ -1,14 +1,15 @@
 <div>
-    <form name="forms" action="https://sep.shaparak.ir/OnlinePG/OnlinePG" method="post">
+    <form name="forms1" action="https://sep.shaparak.ir/OnlinePG/OnlinePG" method="post">
         <input type="hidden" name="Token" value="{{ $token }}" />
         <input name="GetMethod" type="text" value="{{ $getMethod }}" />
     </form>
 
     <script type="text/javascript">
-        // ارسال خودکار فرم پس از بارگذاری صفحه
-        window.onload = function() {
-            document.forms['forms'].submit();
-        };
+        document.addEventListener('DOMContentLoaded', function() {
+            var form = document.forms['forms1'];
+            if (form) {
+                form.submit(); // ارسال فرم تنها در صورتی که فرم وجود داشته باشد
+            }
+        });
     </script>
 </div>
-
